7d02935539390c91379904a928ca86faa7282438
lamseboen <stephenlambok@gmail.com>
2021-07-27 23:41:06 +0700

    fixing delete

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ var dataTodo = []
 
 const showData = payload => {
     let template = `<div class="col-md-3" id="${payload.id}">
-    <div class="card" style="width: 18rem; background-color: ${payload.warna};">
-        <div class="card-body">
-            <h5 class="card-title">${payload.task}</h5>
-            <h6 class="card-title">${payload.tanggal}</h6>
-            <div class="d-flex justify-content-between">
-                            <button class="btn btn-info edit" data-id="${payload.id}"><span class="text-light">&#9998;</span></button>
-                            <button class="btn btn-danger hapus" data-id="${payload.id}"><span class="text-light">&#10006;</span></button>
+                        <div class="card" style="width: 18rem; background-color: ${payload.warna};">
+                            <div class="card-body">
+                                <h5 class="card-title">${payload.task}</h5>
+                                <h6 class="card-title">${payload.tanggal}</h6>
+                                <div class="d-flex justify-content-between">
+                                    <button class="btn btn-info edit" data-id="${payload.id}"><span class="text-light">&#9998;</span></button>
+                                    <button class="btn btn-danger hapus" data-id="${payload.id}"><span class="text-light">&#10006;</span></button>
+                                </div>
+                            </div>
                         </div>
-        </div>
-    </div>
-</div>`
+                    </div>`
     // $("#todo-parent").html(template).show().fadeIn(30000);
     $(template).hide().appendTo('#todo-parent').fadeIn(1000);
 }
 console.log("ready!");
 
 const deleteData = id => {
-    $("#todo-parent").remove(`#${id}`).fadeOut(1000);
+    id = `#${id}`
+    console.log(id)
+    $(`#todo-parent ${id}`).fadeOut(1000, function () {
+        $(this).remove()
+    })
+
 }
 
 $(document).ready(function () {
@@ -31,7 +36,7 @@ $(document).ready(function () {
         let tanggal = $("#tanggal").val()
         let isPenting = $('#penting').is(':checked')
         try {
-            if(task == "") throw "task" 
+            if (task == "") throw "task"
             if (kategori == 0) throw "kategori"
             if (kategori == 1) {
                 warna = "red"
@@ -42,7 +47,7 @@ $(document).ready(function () {
             } else {
                 warna = "green"
             }
-            if(tanggal == "") throw "tanggal"
+            if (tanggal == "") throw "tanggal"
             let id = Math.floor(100000000 + Math.random() * 900000000)
             showData({
                 id,
@@ -51,17 +56,18 @@ $(document).ready(function () {
                 tanggal
             })
             $("#task").val("")
-            $("#kategori").val("0")
+            $("#kategori").val("")
             $("#tanggal").val("")
-            $('#penting').prop('checked', false); 
+            $('#penting').prop('checked', false);
+            $('#collapseExample').collapse("hide")
         } catch (error) {
             console.log(`#${error}`)
             $(`#${error}`).addClass("is-invalid")
-        } 
+        }
     })
 
     $("#todo-parent").on('click', '.hapus', function () {
-        let id = $(".hapus").attr("data-id")
+        let id = $(this).attr("data-id")
         swal({
                 title: "Are you sure?",
                 icon: "warning",
